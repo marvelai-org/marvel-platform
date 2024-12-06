15a5b1c419a699ea0a7c588fa2c0e2e6622fa486
billy <billykbett@gmail.com>
2024-12-06 21:42:37 +0300

    completed AIResistantResponse Output

--- templates/ToolPage/Outputs/index.js
+++ templates/ToolPage/Outputs/index.js
@@ -1,5 +1,6 @@
 import { TOOLS_ID } from '@/constants/tools';
 
+import AIResistantResponse from './AIResistantResponse';
 import FlashCardList from './FlashCardList';
 import QuizResponse from './QuizResponse';
 import SyllabusGeneratorResponse from './SyllabusGeneratorResponse';
@@ -10,6 +11,7 @@ const RESPONSE_OUTPUTS = {
   [TOOLS_ID.QUIZ_GENERATOR]: QuizResponse,
   [TOOLS_ID.WORKSHEET_GENERATOR]: WorksheetGeneratorResponse,
   [TOOLS_ID.SYLLABUS_GENERATOR]: SyllabusGeneratorResponse,
+  [TOOLS_ID.AI_RESISTANCE_ASSIGNMENTS_GENERATOR]: AIResistantResponse,
 };
 
 export default RESPONSE_OUTPUTS;
